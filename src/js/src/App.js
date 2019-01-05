17fea22f53a6150137a55b3f6268df0bc4c1e2d4
Maxime <hardymaxime7@gmail.com>
2019-01-05 11:47:49 +0100

    Ajout loader React

---
+++ src/js/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+class App extends React.Component {
+    render () {
+        return (
+            <main className="container">
+                <h1>Boilerplate build with React & Webpack</h1>
+            </main>
+        )
+    }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,172 @@
+// Variables
+const outputPath = './dist'
+const assetPath = './src'
+const dev = process.env.NODE_ENV !== 'production'
+
+// Dependencies
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const ManifestPlugin = require('webpack-manifest-plugin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// Load configuration from a .env
+// require('dotenv').config({path: path.resolve(process.cwd(), '.env')})
+
+// The brain
+const config = {
+  entry: {
+    main: [`${assetPath}/index.html`, `${assetPath}/css/main.scss`, `${assetPath}/js/main.js`]
+  },
+  mode: dev ? 'development' : 'production',
+  // devtool: dev ? 'cheap-module-eval-source-map' : false,
+  externals: {
+    // jquery: 'jQuery'
+  },
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    },
+    overlay: true,
+    clientLogLevel: 'warning'
+    // host: 0.0.0.0
+  },
+  output: {
+    path: path.resolve(__dirname, outputPath),
+    filename: '[name].js',
+    publicPath: dev ? `http://localhost:8080/` : './'
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.ts', '.tsx'],
+    alias: {
+      '~': path.resolve('node_modules')
+    }
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+       test: /\.(jsx|js)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: !dev
+          }
+        }]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: dev ? 'style-loader' : MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: dev
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: dev,
+              plugins: [
+                require('autoprefixer')({
+                  browsers: ['last 2 versions', 'ie > 8']
+                }),
+                require('css-mqpacker')({
+                  sort: true
+                }),
+                require('cssnano')()
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: dev
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf|wav)(\?.*)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: `[name]${dev ? '' : '.[hash]'}.[ext]`,
+            useRelativePath: !dev
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: `[name]${dev ? '' : '.[hash]'}.[ext]`
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ inject: true, template: 'src/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: `[name]${!dev ? '' : '.[hash]'}.css`,
+      chunkFilename: '[id].css'
+    }),
+    new ImageminPlugin({
+      disable: dev,
+      pngquant: {
+        quality: '95-100'
+      },
+      jpegtran: false,
+      plugins: [
+        imageminMozjpeg({
+          quality: 90,
+          progressive: true
+        })
+      ]
+    }),
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, './'),
+      verbose: true,
+      dry: false
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
+}
+
+// Env specific plugins
+if (dev) {
+  config.plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))// default port 8888
+} else {
+  config.plugins.push(new ManifestPlugin())
+}
+
+module.exports = config
